297f436161551bf9e26a5a108f77de54c3ee269f
Mroboto101 <73853067+Mroboto101@users.noreply.github.com>
2020-11-02 22:51:18 -0300

    ayurame

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path    
-
 from . import views
+from .views import listado_completo
 
 
 app_name='main'
@@ -9,4 +9,5 @@ urlpatterns = [
     path('',views.index, name='index'),
     path('Completos.html/',views.Completos, name='Completos'),
     path('Registro.html/',views.Registro, name='Registro'),
+    path('listado-completos/', listado_completo, name='listado_completo'),
 ]
